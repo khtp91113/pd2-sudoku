f3a7d99a11fb11586c7b43be795eac1aa5b44dd4
khtp91113 <f74036459@mail.ncu.edu.tw>
2015-04-15 22:48:37 +0800

    完成了!!!

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -7,8 +7,7 @@ void Sudoku::GiveQuestion()
 {
  int select,origin[12][12],i,j;
  srand(time(NULL));
- //select=rand()%3;
- select=1;
+ select=rand()%3;//從三個模組中隨機挑一個
  int origin_1[12][12]={{-1,-1,-1, 0, 0, 8, 0, 0, 0, 0, 0, 0},
                        {-1,-1,-1, 0, 0, 4, 0, 9, 0, 0, 0, 1},
                        {-1,-1,-1, 0, 0, 0, 1, 0, 8, 0, 0, 0},
@@ -21,30 +20,30 @@ void Sudoku::GiveQuestion()
                        { 0, 0, 0, 0, 0, 2, 0, 5, 0,-1,-1,-1},
                        { 0, 8, 1, 3, 0, 9, 0, 4, 2,-1,-1,-1},
                        { 0, 5, 0, 7, 1, 0, 0, 0, 0,-1,-1,-1}};
- int origin_2[12][12]={{ 5, 1, 3, 2, 6, 4,-1,-1,-1, 7, 9, 8},
-                       { 7, 6, 4, 8, 1, 9,-1,-1,-1, 2, 3, 5},
-                       { 2, 9, 8, 7, 3, 5,-1,-1,-1, 1, 4, 6},
-                       {-1,-1,-1, 3, 7, 2, 1, 4, 6, 8, 5, 9},
-                       {-1,-1,-1, 5, 9, 6, 2, 3, 8, 4, 7, 1},
-                       {-1,-1,-1, 1, 4, 8, 7, 9, 5, 6, 2, 3},
-                       { 1, 4, 7,-1,-1,-1, 3, 8, 9, 5, 6, 2},
-                       { 3, 2, 6,-1,-1,-1, 4, 5, 1, 9, 8, 7},
-                       { 8, 5, 9,-1,-1,-1, 6, 2, 7, 3, 1, 4},
-                       { 6, 3, 5, 9, 2, 7, 8, 1, 4,-1,-1,-1},
-                       { 9, 7, 2, 4, 8, 1, 5, 6, 3,-1,-1,-1},
-                       { 4, 8, 1, 6, 5, 3, 9, 7, 2,-1,-1,-1}};
- int origin_3[12][12]={{-1,-1,-1, 2, 6, 3, 1, 4, 7, 5, 9, 8},
-                       {-1,-1,-1, 8, 7, 4, 2, 5, 9, 6, 1, 3},
-                       {-1,-1,-1, 5, 9, 1, 8, 3, 6, 2, 4, 7},
-                       { 2, 3, 7,-1,-1,-1, 6, 1, 4, 9, 8, 5},
-                       { 5, 8, 1,-1,-1,-1, 7, 9, 2, 4, 3, 6},
-                       { 4, 6, 9,-1,-1,-1, 3, 8, 5, 7, 2, 1},
-                       { 9, 2, 8, 3, 5, 7,-1,-1,-1, 1, 6, 4},
-                       { 1, 5, 3, 4, 2, 6,-1,-1,-1, 8, 7, 9},
-                       { 7, 4, 6, 1, 8, 9,-1,-1,-1, 3, 5, 2},
-                       { 6, 7, 5, 9, 3, 8, 4, 2, 1,-1,-1,-1},
-                       { 8, 9, 4, 7, 1, 2, 5, 6, 3,-1,-1,-1},
-                       { 3, 1, 2, 6, 4, 5, 9, 7, 8,-1,-1,-1}};
+ int origin_2[12][12]={{ 0, 0, 0, 0, 0, 0,-1,-1,-1, 0, 0, 0},
+                       { 0, 0, 0, 0, 1, 0,-1,-1,-1, 2, 3, 5},
+                       { 0, 0, 0, 0, 3, 0,-1,-1,-1, 1, 4, 0},
+                       {-1,-1,-1, 0, 0, 0, 0, 0, 0, 0, 5, 0},
+                       {-1,-1,-1, 0, 0, 6, 0, 0, 8, 0, 7, 0},
+                       {-1,-1,-1, 0, 0, 0, 0, 9, 0, 0, 0, 0},
+                       { 0, 0, 7,-1,-1,-1, 0, 0, 0, 0, 0, 0},
+                       { 0, 0, 6,-1,-1,-1, 4, 0, 1, 9, 0, 7},
+                       { 0, 0, 0,-1,-1,-1, 0, 2, 0, 3, 1, 0},
+                       { 0, 3, 0, 0, 2, 0, 0, 1, 0,-1,-1,-1},
+                       { 9, 7, 0, 4, 0, 0, 5, 0, 0,-1,-1,-1},
+                       { 4, 0, 0, 0, 5, 3, 0, 0, 0,-1,-1,-1}};
+ int origin_3[12][12]={{-1,-1,-1, 0, 0, 0, 0, 4, 0, 5, 9, 8},
+                       {-1,-1,-1, 0, 7, 4, 2, 0, 9, 6, 0, 3},
+                       {-1,-1,-1, 0, 0, 0, 0, 3, 0, 0, 4, 0},
+                       { 0, 3, 0,-1,-1,-1, 0, 0, 4, 9, 0, 0},
+                       { 5, 0, 0,-1,-1,-1, 0, 0, 0, 0, 0, 6},
+                       { 0, 0, 0,-1,-1,-1, 0, 0, 0, 0, 0, 0},
+                       { 9, 2, 0, 0, 0, 0,-1,-1,-1, 0, 0, 0},
+                       { 0, 0, 0, 0, 0, 0,-1,-1,-1, 0, 7, 9},
+                       { 0, 4, 0, 1, 0, 0,-1,-1,-1, 0, 5, 0},
+                       { 0, 7, 0, 0, 3, 0, 0, 0, 0,-1,-1,-1},
+                       { 8, 0, 0, 0, 0, 2, 5, 6, 0,-1,-1,-1},
+                       { 0, 1, 2, 0, 4, 0, 0, 7, 0,-1,-1,-1}};
  switch(select)
  {
   case 0:for(i=0;i<12;i++)
@@ -78,7 +77,7 @@ void Sudoku::GiveQuestion()
   }
  int temp[12][12];
  srand(time(NULL));
- rotate=rand()%5;
+ rotate=rand()%5;//旋轉 有可能是行列互換 或上下左右顛倒
  switch(rotate)
  {
   case 0:for(i=0;i<12;i++)
@@ -111,7 +110,7 @@ void Sudoku::GiveQuestion()
          break;
   default:break;
  }
- 
+ //stdoutput
  for(i=0;i<12;i++)
  {
   for(j=0;j<12;j++)
@@ -127,7 +126,7 @@ void Sudoku::GiveQuestion()
 
 void Sudoku::ReadIn()
 {
- int i,j;
+ int i,j;//stdinput
  for(i=0;i<12;i++)
   for(j=0;j<12;j++)
    cin>>sudoku[i][j];
@@ -135,8 +134,8 @@ void Sudoku::ReadIn()
 
 int Sudoku::correct(int result)
 {
- int i,j,k,check[9]={0};
- for(i=0;i<12;i++)
+ int i,j,k,check[9]={0};//檢查每一行或列和九宮格中除了0和-1外 有沒有重複的數字
+ for(i=0;i<12;i++)//列
  {
   for(j=0;j<12;j++)
   {
@@ -151,7 +150,7 @@ int Sudoku::correct(int result)
   }
   for(j=0;j<9;j++)
    check[j]=0;
- }
+ }//行
  for(i=0;i<12;i++)
  {
   for(j=0;j<12;j++)
@@ -168,7 +167,7 @@ int Sudoku::correct(int result)
   for(j=0;j<9;j++)
    check[j]=0;
  }
- int temp1,temp2;
+ int temp1,temp2;//九宮格
  for(i=0;i<4;i++)
   for(j=0;j<4;j++)
   {
@@ -200,23 +199,28 @@ int Sudoku::correct(int result)
 
 void Sudoku::Backtracking(int num,int x[],int t[],int m[],int n[],int count,int possible[][9])
 {
- int no=0,i,j;
+ int no=0,i,j;//看還有沒有空格沒填
  for(i=0;i<12;i++)
   for(j=0;j<12;j++)
    if(sudoku[i][j]==0)
     no=1;
- if(no==0&&multi==1)
+ if(no==0&&multi[0]==1)//如果沒有空格且已出現過一次解答 那代表多重解 output 2 並結束程式
  {
   cout<<2<<endl;
   exit(0);
  }
 
- if(no==0)
+ if(no==0)//沒有空格 第一次出現解答 改multi的值 並將解答暫存到tem中
  {
-  multi=1;
+  multi[0]=1;
+  for(i=0;i<12;i++)
+   for(j=0;j<12;j++)
+    tem[i][j]=sudoku[i][j];
   return;
  }
- for(;t[num]<x[num];t[num]++)
+
+
+ for(;t[num]<x[num];t[num]++)//recursion 如果填進去的數字符合規則 就在填下一格 不對就return回上一層
  {
   sudoku[m[num]][n[num]]=possible[num][t[num]];
   if(correct(-1)==-1)
@@ -230,7 +234,7 @@ void Sudoku::Backtracking(int num,int x[],int t[],int m[],int n[],int count,int 
 void Sudoku::Solve()
 {
  int result=-1,i,j,k,total=0,pass=0,check[9]={0};
- for(i=0;i<12;i++)
+ for(i=0;i<12;i++)//檢查-1的位置有沒有正確
   for(j=0;j<12;j++)
   {
    if(sudoku[i][j]==-1)
@@ -247,16 +251,16 @@ void Sudoku::Solve()
     else;
    }
   }
- if(total!=36)
+ if(total!=36)//檢查-1的數量有沒有正確
   result=0;
  else;
- result=correct(result);
- if(result==0)
+ result=correct(result);//看數字有沒有符合規則
+ if(result==0)//不正確 output 0 結束程式
  {
   cout<<result<<endl;
   return;
  }
- int count=0;
+ int count=0;//記錄所有空格的位置 和空格數量
  for(i=0;i<12;i++)
   for(j=0;j<12;j++)
    position[i][j]=0;
@@ -268,7 +272,7 @@ void Sudoku::Solve()
     position[i][j]++;
    }
  int b=0,c=0,only=0,control=1;
- while(control==1)
+ while(control==1)//將只有一種可能的空格先填好
  {
   control=0;
   for(i=0;i<12;i++)
@@ -499,7 +503,7 @@ void Sudoku::Solve()
   for(j=0;j<12;j++)
    if(sudoku[i][j]==0)
     clear=0;
- if(clear==1)
+ if(clear==1)//沒有空格的情況下
  {
   result=correct(result);
   if(result==-1)
@@ -532,7 +536,7 @@ void Sudoku::Solve()
   for(j=0;j<9;j++)
    possible[i][j]=0;
  control=1;
- while(control==1)
+ while(control==1)//重新存還剩多少空格 和空格的位置
  {
   control=0;
   count=0;
@@ -543,7 +547,7 @@ void Sudoku::Solve()
   for(i=0;i<12;i++)
   {
    for(j=0;j<12;j++)
-    if(position[i][j]==1)
+    if(position[i][j]==1)//在空格中 去找這個空格所有可以填的數字 藉由比較行 列 九宮格 三種情況下 皆符合規定的數字就是有可能的數字 存到possible中
     {
      note=0;
      for(k=0;k<9;k++)
@@ -639,7 +643,7 @@ void Sudoku::Solve()
    } 
   }
  }
- int impossible=0;
+ int impossible=0;//如果有一個空格完全沒有可能的答案 那就代表無解 結束程式
  for(i=0;i<count;i++)
  {
   for(j=0;j<9;j++)
@@ -653,39 +657,8 @@ void Sudoku::Solve()
   impossible=0;
  }
 
- //檢查還有沒有空格
- clear=1;
- for(i=0;i<12;i++)
-  for(j=0;j<12;j++)
-   if(sudoku[i][j]==0)
-    clear=0;
- if(clear==1)
- {
-  result=correct(result);
-  if(result==-1)
-  {
-   cout<<"1"<<endl;//全部剛好能一次解決的沒有不確定答案加上沒有不符合規定
-   for(i=0;i<12;i++)
-   {
-    for(j=0;j<12;j++)
-    {
-     if(sudoku[i][j]==-1)
-      cout<<sudoku[i][j]<<" ";
-     else
-      cout<<" "<<sudoku[i][j]<<" ";
-    }
-    cout<<endl;
-   } 
-   return;
-  }
-  else
-  {
-   cout<<"0"<<endl;//全部剛好能一次解決但不符合規定
-   return;
-  }
- }
  k=0;
- int m[count1],n[count1];
+ int m[count1],n[count1];//m和n 分別存空格的列和行的值
  for(i=0;i<12;i++)
   for(j=0;j<12;j++)
   {
@@ -697,25 +670,36 @@ void Sudoku::Solve()
     k++;
    }
   }
- int x[count],t[count],ch=possible[0][0],total1[count],q,temp,multi=0;
- for(i=0;i<count;i++)
+ int x[count],t[count];
+ for(i=0;i<count;i++)//t陣列 ex:t[0] 表示第一個空格此時在他的可能數字的第幾個 比如說 如果第一格有3個可能性 那t[0]=1時 就是說把第一個空格填所有可能中的第二個可能的數字
   t[i]=0;
- for(i=0;i<count;i++)
-  total1[i]=0;
- for(i=0;i<count;i++)
+ for(i=0;i<count;i++)//x陣列存每個空格中的可能數字有幾個 當作範圍使用
   x[i]=0;
  for(i=0;i<count;i++)
   for(j=0;j<9&&possible[i][j]!=0;j++)
    x[i]++;
- for(i=0;i<count;i++)
-  total1[i]=possible[i][t[i]];
-
- multi=0;
+ multi[0]=0;
  Backtracking(0,x,t,m,n,count,possible);
- 
- if(multi==1)
+ if(multi[0]==1)//multi為一 代表唯一解 output tem
  {
   cout<<1<<endl;
+  for(i=0;i<12;i++)
+  {
+   for(j=0;j<12;j++)
+   {
+    if(tem[i][j]==-1)
+     cout<<tem[i][j]<<" ";
+    else
+     cout<<" "<<tem[i][j]<<" ";
+   }
+   cout<<endl;
+  }
+  return;
+ }
+
+ if(multi[0]==0)//multi為零 代表無解 結束程式
+ {
+  cout<<0<<endl;
   for(i=0;i<12;i++)
   {
    for(j=0;j<12;j++)
@@ -728,11 +712,6 @@ void Sudoku::Solve()
    cout<<endl;
   }
   return;
- }
-
- if(multi==0)
- {
-  cout<<0<<endl;
-  return;
  } 
+ return;
 }
--- Sudoku.h
+++ Sudoku.h
@@ -2,15 +2,14 @@
 class Sudoku
 {
  public:
-   void GiveQuestion();
-   int correct(int result);
-   void ReadIn();
-   int Multiple(int total[],int count,int m[],int n[]);
-   void Solve();
-   void Backtracking(int num,int x[],int t[],int m[],int n[],int count,int possible[][9]);
+   void GiveQuestion();//出題
+   int correct(int result);//檢查有沒有符合規則
+   void ReadIn();//讀題
+   void Solve();//解題
+   void Backtracking(int num,int x[],int t[],int m[],int n[],int count,int possible[][9]);//暴力解題
  private:
-   int sudoku[12][12];
-   int position[12][12];
-   int tem[12][12];
-   int multi;
+   int sudoku[12][12];//存題目
+   int position[12][12];//存空格位置
+   int tem[12][12];//不重要
+   int multi[1];//用來看是多重解還是唯一解
 };
